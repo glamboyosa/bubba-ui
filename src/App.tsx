2f3d1d10afa11073ad7d40d22105f7facb02e446
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2022-02-24 14:40:13 +0100

    feat: change game rules

--- src/App.tsx
+++ src/App.tsx
@@ -82,7 +82,7 @@ function App() {
           )
           setStart(false)
           setSolved(true)
-        } else if (!result.solved && result.attempts === 6) {
+        } else if (!result.solved && result.attempts === 3) {
           setStart(false)
           stop()
           toast(
